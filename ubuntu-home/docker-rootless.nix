bb8a1f183dee13793d2bd7bdd48066dc578b8b87
Ubuntu User <ubuntu@user.com>
2024-12-05 14:46:24 +0000

    trying docker again

--- ubuntu-home/docker-rootless.nix
+++ ubuntu-home/docker-rootless.nix
@@ -4,7 +4,7 @@
     services.podman = {
         enable = true;
         # Create a `docker` alias for podman, to use it as a drop-in replacement
-        dockerCompat = true;
+        # dockerCompat = true;
         # Required for containers under podman compose to be able to talk to each other.
         # defaultNetwork.settings.dns_enabled = true;
 
