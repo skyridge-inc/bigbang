cf7d920fb19be5d7474ec2fedc67172091eb13e7
Ubuntu User <ubuntu@user.com>
2024-12-05 14:45:54 +0000

    trying docker again

--- ubuntu-home/docker-rootless.nix
+++ ubuntu-home/docker-rootless.nix
@@ -6,11 +6,11 @@
         # Create a `docker` alias for podman, to use it as a drop-in replacement
         dockerCompat = true;
         # Required for containers under podman compose to be able to talk to each other.
-        defaultNetwork.settings.dns_enabled = true;
+        # defaultNetwork.settings.dns_enabled = true;
 
         # Optional: Automatically add your user to the docker group
         # This requires you to log out and back in for group changes to take effect
-        userGroups = [ "docker" ];
+        # userGroups = [ "docker" ];
     };
 
 }
