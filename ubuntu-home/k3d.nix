e750f19eaf7d8c09c8916a57756eb422b82fff99
Ubuntu User <ubuntu@user.com>
2024-12-05 19:12:02 +0000

    added docker alias

--- ubuntu-home/k3d.nix
+++ ubuntu-home/k3d.nix
@@ -7,17 +7,16 @@ let
     version = "5.4.8";  # Replace with the desired version
 
     # Fetch the k3d binary
-#    src = pkgs.fetchurl {
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh";
 #      url = "https://github.com/k3d-io/k3d/releases/download/v${version}/k3d-linux-amd64";
 #      sha256 = "0ys1wh24dl7gql4i5srjpamcf2nhsqsbs5z4i0b72h5a57fv9m6w";  # Update with the correct sha256
-#    };
+    };
 
     # Installation steps
     installPhase = ''
       mkdir -p $out/bin
-
-      curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | K3D_INSTALL_DIR="$out/bin" bash
-
+      cat $src | K3D_INSTALL_DIR="$out/bin" bash
 #      install -m755 $src $out/bin/k3d
     '';
 
