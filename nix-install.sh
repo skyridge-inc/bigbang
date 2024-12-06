7b478581343b21aa3f56da1cd47f391d5b289caa
Ubuntu User <ubuntu@user.com>
2024-12-06 06:13:51 +0000

    trying k3d pkg

--- nix-install.sh
+++ nix-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-## Already installed?
-# nix --version 2> /dev/null \
-#     && echo "Nix is already INSTALLED!  Exiting" && exit 0
-
 # Install Nix multi-user
 # REF:  https://nixos.org/download/#nix-install-linux
 sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# Set up /etc/nix/nix.conf
+sudo mkdir -p /etc/nix
+sudo printf "build-users-group = nixbld\nexperimental-features = nix-command flakes\n" | sudo tee /etc/nix/nix.conf
