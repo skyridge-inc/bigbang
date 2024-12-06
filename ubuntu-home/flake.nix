39e3304f113fd67668d0551b5676084275d13efc
Ubuntu User <ubuntu@user.com>
2024-12-06 05:10:14 +0000

    trying k3d pkg

--- ubuntu-home/flake.nix
+++ ubuntu-home/flake.nix
@@ -20,13 +20,22 @@
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
+        # REF:  https://repo1.dso.mil/big-bang/bigbang/-/blob/master/docs/guides/deployment-scenarios/quickstart.md#step-1-provision-a-virtual-machine
         modules = [ 
+          # PRE-INSTALL:  Install home-manager nix pkg
           ./home.nix
-          ./git.nix
+          # PRE-INSTALL:  Install zsh with oh-my-zsh and plugins  YEA!
           ./zsh.nix
+          # STEP 1:  Install git
+          ./git.nix
+          # STEP 2/3/4:  Install podman (not docker) and alias docker to podman
           ./docker-rootless.nix
+          # STEP 5/6:  Install k3d
           ./k3d.nix
+          # STEP 7/8:  Install kubectl
           ./kubectl.nix
+          # STEP 9/10:  Install kustomize
+          ./kustomize.nix
         ];
 
         # Optionally use extraSpecialArgs
